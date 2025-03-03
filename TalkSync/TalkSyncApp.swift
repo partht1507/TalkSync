dc9e3081f9e7ceeec6123b26597cb9464bed9520
parth1507 <parthh1507@gmail.com>
2025-03-03 17:12:24 -0500

    Initial Commit

---
+++ TalkSync/TalkSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkSyncApp.swift
+//  TalkSync
+//
+//  Created by Parth Thakkar on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct TalkSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TalkSyncTests/TalkSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  TalkSyncTests.swift
+//  TalkSyncTests
+//
+//  Created by Parth Thakkar on 2025-03-03.
+//
+
+import Testing
+@testable import TalkSync
+
+struct TalkSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
